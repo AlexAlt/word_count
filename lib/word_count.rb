3d625e0b236c01bd221e26d4d8c0f15ec03eaec7
AlexAlt <alexandra.m.altieri@gmail.com>
2015-08-07 09:35:47 -0700

    Creates first spec and adds code to pass first spec

---
+++ lib/word_count.rb
@@ -0,0 +1,14 @@
+class String
+  define_method(:word_count) do |target_word|
+    word_count_array = self.split(" ")
+
+    target_count = 0
+
+    word_count_array.each() do |index|
+      if index == target_word
+        target_count = target_count.+(1)
+      end
+    end
+    target_count
+  end
+end
