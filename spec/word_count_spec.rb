69c932a5914a1e6cda9d96c1300298ba38258cf1
AlexAlt <alexandra.m.altieri@gmail.com>
2015-08-07 09:28:05 -0700

    Adds initial files

---
+++ spec/word_count_spec.rb
@@ -0,0 +1,8 @@
+require('rspec')
+require('./lib/word_count.rb')
+
+describe('String#word_count') do
+  it('counts the number of target words in a multiple word string') do
+    expect("cat dog bird cat dog".word_count("cat")).to(eq(2))
+  end
+end
