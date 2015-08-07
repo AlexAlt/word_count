b329c26041afda8aeb9075307d9d08c06452c53b
AlexAlt <alexandra.m.altieri@gmail.com>
2015-08-07 10:44:23 -0700

    Adjusts specs to include expectation for filling in the word_count field

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -1,13 +1,14 @@
 require('capybara/rspec')
 require('./app')
-Capybara.app = Sinatra::Applications
+Capybara.app = Sinatra::Application
 set(:show_expectations, false)
 
 describe('the word count process', {:type => :feature}) do
   it('processes the user entry of word string and target word and returns the number of instances of target word') do
     visit('/')
     fill_in("target_word", :with => "cat")
+    fill_in("word_string", :with => "cat, dog, cat, bird")
     click_button("Count that Ish")
-    expect(page).to have_content("2")
+    expect(page).to have_content("This word shows up 2 times!!!")
   end
 end
