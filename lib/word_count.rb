cc36c8c0a41288c2cdf1bc9e9c1d4072d1bb91cf
AlexAlt <alexandra.m.altieri@gmail.com>
2015-08-07 09:56:53 -0700

    Adds second spec to manage non-letter characters; passes spec.

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -5,8 +5,15 @@ class String
     target_count = 0
 
     word_count_array.each() do |index|
-      if index == target_word
-        target_count = target_count.+(1)
+      if index.include?(",")
+        new_index = index.chomp(",")
+        if new_index == target_word
+          target_count = target_count.+(1)
+        end
+      else 
+        if index == target_word
+          target_count = target_count.+(1)
+        end
       end
     end
     target_count
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -5,4 +5,8 @@ describe('String#word_count') do
   it('counts the number of target words in a multiple word string') do
     expect("cat dog bird cat dog".word_count("cat")).to(eq(2))
   end
+
+  it('manages non-letter characters') do
+    expect("cat, dog, bird, cat, dog".word_count("cat")).to(eq(2))
+  end
 end
